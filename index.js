6f58fd16e1b2f36811df3b284c60ec4372bb7055
sauravkhambayate1 <sauravkhambayate122@gmail.com>
2023-06-12 19:00:08 +0530

    second commit

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const dotenv=require('dotenv')
 dotenv.config();
 const express  = require('express')
 const app = express()
-const cors = require('cors')
+
 const credentialRoutes = require('./Routers/routes')
 const connect = require('./database/db')
 app.use(express.json())
 app.use(credentialRoutes)
-app.use(cors)
+
 app.listen(3005, ()=>{
     connect()
     console.log("Server running on Port 3005");
